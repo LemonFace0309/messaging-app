0ec88e2901b2f35e2ffe2163183fa71a3a0713bf
Charles Liu <charles.liu0309@gmail.com>
2021-05-09 02:05:08 -0400

    created contact sidebar and contact context

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import Login from './Login'
 import Dashboard from './Dashboard'
 import useLocalStorage from '../hooks/useLocalStorage'
+import { ContactsProvider } from '../contexts/ContactsProvider'
 
 function App() {
   const [id, setId] = useLocalStorage('id')
 
-  return id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />
+  const dashbaord = (
+    <ContactsProvider>
+      <Dashboard id={id} />
+    </ContactsProvider>
+  )
+
+  return id ? dashbaord : <Login onIdSubmit={setId} />
 }
 
 export default App
