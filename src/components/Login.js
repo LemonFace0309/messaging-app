a151c6226e11cf14b76881fd425dbb119c7dbb0c
Charles Liu <charles.liu0309@gmail.com>
2021-05-09 00:56:43 -0400

    created login page

---
+++ src/components/Login.js
@@ -0,0 +1,27 @@
+import React, { useRef } from 'react'
+import { Container, Form, Button } from 'react-bootstrap'
+
+function Login({onIdSubmit}) {
+  const idRef = useRef()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    onIdSubmit(idRef.current.value)
+  }
+
+  return (
+    <Container className="align-items-center d-flex" style={{ height: '100vh' }}>
+      <Form onSubmit={handleSubmit} className="w-100">
+        <Form.Group>
+          <Form.Label>Enter Your Id</Form.Label>
+          <Form.Control type="text" ref={idRef} required />
+        </Form.Group>
+        <Button type="submit">Login</Button>
+        <Button variant="secondary">Create a New Id</Button>
+      </Form>
+    </Container>
+  )
+}
+
+export default Login
