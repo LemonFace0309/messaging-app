fc8f8eb9b53871bfd6b0bf5a61adf72d0821f98f
Charles Liu <charles.liu0309@gmail.com>
2021-05-09 02:52:29 -0400

    created conversation sidebar and context

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,17 @@ import Login from './Login'
 import Dashboard from './Dashboard'
 import useLocalStorage from '../hooks/useLocalStorage'
 import { ContactsProvider } from '../contexts/ContactsProvider'
+import { ConversationsProvider } from '../contexts/ConversationsProvider'
 
 function App() {
   const [id, setId] = useLocalStorage('id')
 
   const dashbaord = (
     <ContactsProvider>
+    <ConversationsProvider id={id}>
       <Dashboard id={id} />
-    </ContactsProvider>
+    </ConversationsProvider>
+  </ContactsProvider>
   )
 
   return id ? dashbaord : <Login onIdSubmit={setId} />
